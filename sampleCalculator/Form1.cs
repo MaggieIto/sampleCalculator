2bd7f82d5352908572865c89045e942e21ede7ec
Maggie Ito <kaz21drama@gmail.com>
2020-02-26 01:29:45 +0900

    変数の名前を変更 Result>result, Input_str>input_str, Operator>operatorBtn

--- sampleCalculator/Form1.cs
+++ sampleCalculator/Form1.cs
@@ -16,9 +16,9 @@ namespace sampleCalculator
         {
             InitializeComponent();
         }
-        string Input_str = "";
-        double Result = 0;
-        string Operator = null;
+        string input_str = "";
+        double result = 0;
+        string operatorBtn = null;
 
         // 0～9, 00と.の計12個のボタンが対象
         private void button1_Click_1(object sender, EventArgs e)
@@ -30,50 +30,50 @@ namespace sampleCalculator
             string text = btn.Text;
 
             // 既に入力された文字と新規入力文字と、両方に小数点がある場合、表示をスキップする
-            if (!(Input_str.Contains(".") && text.Contains(".")))
+            if (!(input_str.Contains(".") && text.Contains(".")))
             {
                 // [入力された数字]に連結する
-                Input_str += text;
+                input_str += text;
             }
             // 画面上に数字を出す
-            textBox1.Text = Input_str;
+            textBox1.Text = input_str;
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
-            double num1 = Result;
+            double num1 = result;
             double num2;
 
             // 入力文字が空欄の場合、計算をスキップする
-            if (Input_str != "")
+            if (input_str != "")
             {
                 // 入力文字を数字に変換
-                num2 = double.Parse(Input_str);
+                num2 = double.Parse(input_str);
                 // 四則演算
-                if (Operator == "＋")
-                    Result = num1 + num2;
-                if (Operator == "-")
-                    Result = num1 - num2;
-                if (Operator == "×")
-                    Result = num1 * num2;
-                if (Operator == "÷")
-                    Result = num1 / num2;
+                if (operatorBtn == "＋")
+                    result = num1 + num2;
+                if (operatorBtn == "-")
+                    result = num1 - num2;
+                if (operatorBtn == "×")
+                    result = num1 * num2;
+                if (operatorBtn == "÷")
+                    result = num1 / num2;
 
                 // 演算子が押されなかった場合、入力文字を結果とする
-                if (Operator == null)
-                    Result = num2;
+                if (operatorBtn == null)
+                    result = num2;
             }
             // 画面に計算結果を表示
-            textBox1.Text = Result.ToString();
+            textBox1.Text = result.ToString();
 
             // 今入力されている数字をリセットする
-            Input_str = "";
-            // 演算子をOperator変数に入れる
+            input_str = "";
+            // 演算子をoperatorBtn変数に入れる
             Button btnOpe = (Button)sender;
-            Operator = btnOpe.Text;
+            operatorBtn = btnOpe.Text;
 
-            if (Operator == "=")
-                Operator = "";
+            if (operatorBtn == "=")
+                operatorBtn = "";
         }
 
         private void button19_Click(object sender, EventArgs e)
